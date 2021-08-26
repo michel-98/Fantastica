f947bec9450a3ccf72a455dd76f25ab8d94985c1
Michele Antonacci <michele.antonacci1098@gmail.com>
2021-08-26 21:30:29 +0200

    Dati in real time
    inserire possibilità di cambiare modulo e istruzioni per cors enabler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { WebSocketAPI } from './WebSocketAPI';
 
 @Component({
   selector: 'app-root',
@@ -7,28 +6,5 @@ import { WebSocketAPI } from './WebSocketAPI';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular8-springboot-websocket';
 
-  greeting: any;
-  name: string;
-  constructor(public webSocketAPI: WebSocketAPI){}
-
-  ngOnInit() {
-  }
-
-  connect(){
-    this.webSocketAPI._connect();
-  }
-
-  disconnect(){
-    this.webSocketAPI._disconnect();
-  }
-
-  sendMessage(){
-    this.webSocketAPI._send(this.name);
-  }
-
-  handleMessage(message){
-    this.greeting = message;
-  }
 }
---
+++ src/app/new/HTMLPADRE.ts
@@ -0,0 +1,666 @@
+
+export class Giocatore{
+  nome: string;
+  percentuale: number;
+  ruolo:string;
+}
+
+export class Giocatore2{
+  nome: string;
+  ruolo:string;
+}
+
+
+export  interface HTMLPADRE {
+  type: string;
+  DIV: DIV6[];
+  attributes: Attributes10;
+}
+
+export interface Attributes10 {
+  id: string;
+  itemscope: string;
+  itemtype: string;
+  class: string;
+  lazy: string;
+}
+
+export interface DIV6 {
+  type: string;
+  DIV?: DIV5[];
+  attributes?: Attributes8;
+  HEADER?: HEADER2[];
+}
+
+export interface HEADER2 {
+  type: string;
+  SPAN: string[];
+  attributes: Attributes9;
+}
+
+export interface Attributes9 {
+  class: string;
+  label: string;
+}
+
+export interface Attributes8 {
+  class: string;
+  'match-card'?: string;
+  'p-2'?: string;
+  paper?: string;
+  _lazy: string;
+  'row-responsive'?: string;
+  comments?: string;
+  'g-2'?: string;
+  'pb-4'?: string;
+  'mb-3'?: string;
+  'd-flex'?: string;
+  lineups?: string;
+  reserves?: string;
+}
+
+export interface DIV5 {
+  type: string;
+  HEADER?: HEADER[];
+  attributes: Attributes3;
+  DIV?: DIV4[];
+  FOOTER?: string[];
+}
+
+export interface DIV4 {
+  type: string;
+  H2?: string[];
+  attributes?: Attributes4;
+  P?: string[];
+  A?: A[];
+  SPAN?: string[];
+  DIV?: DIV3[];
+}
+
+export interface DIV3 {
+  type: string;
+  P: (P | P2 | string)[];
+}
+
+export interface P2 {
+  type: string;
+  B: string[];
+}
+
+export interface P {
+  type: string;
+}
+
+export interface A {
+  type: string;
+  SPAN?: string[];
+  attributes: Attributes5;
+  DIV?: DIV2[];
+}
+
+export interface DIV2 {
+  type: string;
+  DIV: DIV[];
+  attributes: Attributes7;
+}
+
+export interface Attributes7 {
+  class: string;
+  role: string;
+  style: string;
+  '90%'?: string;
+  'aria-label': string;
+  di: string;
+  silvestri?: string;
+  'aria-valuenow': string;
+  'aria-valuemin': string;
+  'aria-valuemax': string;
+  'data-value': string;
+  '55%'?: string;
+  rodrigo?: string;
+  becao?: string;
+  nuytinck?: string;
+  samir?: string;
+  molina?: string;
+  'n.'?: string;
+  pereyra?: string;
+  '75%'?: string;
+  arslan?: string;
+  '80%'?: string;
+  walace?: string;
+  '85%'?: string;
+  udogie?: string;
+  deulofeu?: string;
+  okaka?: string;
+  '70%'?: string;
+  maenpaa?: string;
+  ebuehi?: string;
+  caldara?: string;
+  ceccaroni?: string;
+  molinaro?: string;
+  '60%'?: string;
+  fiordilino?: string;
+  peretz?: string;
+  heymans?: string;
+  johnsen?: string;
+  forte?: string;
+  mariano?: string;
+  '5%'?: string;
+  scuffet?: string;
+  '1%'?: string;
+  padelli?: string;
+  '50%'?: string;
+  stryger?: string;
+  larsen?: string;
+  '35%'?: string;
+  de?: string;
+  maio?: string;
+  zeegelaar?: string;
+  '40%'?: string;
+  samardzic?: string;
+  jajalo?: string;
+  makengo?: string;
+  '15%'?: string;
+  palumbo?: string;
+  pussetto?: string;
+  lezzerini?: string;
+  bertinato?: string;
+  svoboda?: string;
+  schnegg?: string;
+  '25%'?: string;
+  mazzocchi?: string;
+  modolo?: string;
+  tessmann?: string;
+  '30%'?: string;
+  vacca?: string;
+  dezi?: string;
+  sigurdsson?: string;
+  'a.'?: string;
+  okereke?: string;
+  henry?: string;
+  'montipo&#39;'?: string;
+  gunter?: string;
+  ceccherini?: string;
+  dawidowicz?: string;
+  faraoni?: string;
+  ilic?: string;
+  tameze?: string;
+  lazovic?: string;
+  barak?: string;
+  zaccagni?: string;
+  kalinic?: string;
+  handanovic?: string;
+  skriniar?: string;
+  vrij?: string;
+  bastoni?: string;
+  darmian?: string;
+  barella?: string;
+  brozovic?: string;
+  calhanoglu?: string;
+  '65%'?: string;
+  perisic?: string;
+  martinez?: string;
+  'l.'?: string;
+  dzeko?: string;
+  pandur?: string;
+  berardi?: string;
+  magnani?: string;
+  '20%'?: string;
+  cetin?: string;
+  ruegg?: string;
+  sutalo?: string;
+  casale?: string;
+  frabotta?: string;
+  hongla?: string;
+  bessa?: string;
+  cancellieri?: string;
+  simeone?: string;
+  radu?: string;
+  'i.'?: string;
+  cordaz?: string;
+  dimarco?: string;
+  ranocchia?: string;
+  kolarov?: string;
+  'd&#39;ambrosio'?: string;
+  dumfries?: string;
+  vecino?: string;
+  vidal?: string;
+  sensi?: string;
+  correa?: string;
+  satriano?: string;
+  musso?: string;
+  toloi?: string;
+  demiral?: string;
+  djimsiti?: string;
+  maehle?: string;
+  pessina?: string;
+  freuler?: string;
+  gosens?: string;
+  malinovskyi?: string;
+  ilicic?: string;
+  muriel?: string;
+  skorupski?: string;
+  soumaoro?: string;
+  bonifazi?: string;
+  hickey?: string;
+  svanberg?: string;
+  dominguez?: string;
+  orsolini?: string;
+  vignato?: string;
+  barrow?: string;
+  arnautovic?: string;
+  sportiello?: string;
+  rossi?: string;
+  'f.'?: string;
+  lovato?: string;
+  pezzella?: string;
+  'giu.'?: string;
+  scalvini?: string;
+  palomino?: string;
+  zappacosta?: string;
+  miranchuk?: string;
+  pasalic?: string;
+  piccoli?: string;
+  lammers?: string;
+  bardi?: string;
+  mbaye?: string;
+  binks?: string;
+  tomiyasu?: string;
+  medel?: string;
+  skov?: string;
+  olsen?: string;
+  sansone?: string;
+  santander?: string;
+  van?: string;
+  hooijdonk?: string;
+  reina?: string;
+  lazzari?: string;
+  luiz?: string;
+  felipe?: string;
+  acerbi?: string;
+  hysaj?: string;
+  'milinkovic-savic'?: string;
+  lucas?: string;
+  leiva?: string;
+  luis?: string;
+  alberto?: string;
+  pedro?: string;
+  immobile?: string;
+  anderson?: string;
+  zoet?: string;
+  hristov?: string;
+  erlic?: string;
+  nikolaou?: string;
+  amian?: string;
+  maggiore?: string;
+  kovalenko?: string;
+  's.'?: string;
+  verde?: string;
+  colley?: string;
+  'e.'?: string;
+  gyasi?: string;
+  strakosha?: string;
+  marusic?: string;
+  patric?: string;
+  romero?: string;
+  escalante?: string;
+  basic?: string;
+  '10%'?: string;
+  cataldi?: string;
+  akpa?: string;
+  akpro?: string;
+  moro?: string;
+  muriqi?: string;
+  provedel?: string;
+  zovko?: string;
+  vignali?: string;
+  sala?: string;
+  ferrer?: string;
+  sher?: string;
+  mraz?: string;
+  antiste?: string;
+  terracciano?: string;
+  venuti?: string;
+  milenkovic?: string;
+  nastasic?: string;
+  biraghi?: string;
+  bonaventura?: string;
+  pulgar?: string;
+  castrovilli?: string;
+  callejon?: string;
+  vlahovic?: string;
+  gonzalez?: string;
+  'v.'?: string;
+  izzo?: string;
+  bremer?: string;
+  rodriguez?: string;
+  'r.'?: string;
+  singo?: string;
+  mandragora?: string;
+  lukic?: string;
+  aina?: string;
+  linetty?: string;
+  pjaca?: string;
+  belotti?: string;
+  rosati?: string;
+  quarta?: string;
+  terzic?: string;
+  igor?: string;
+  benassi?: string;
+  bianco?: string;
+  torreira?: string;
+  duncan?: string;
+  maleh?: string;
+  sottil?: string;
+  saponara?: string;
+  kokorin?: string;
+  berisha?: string;
+  gemello?: string;
+  djidji?: string;
+  vojvoda?: string;
+  buongiorno?: string;
+  ansaldi?: string;
+  rincon?: string;
+  verdi?: string;
+  baselli?: string;
+  segre?: string;
+  warming?: string;
+  sanabria?: string;
+  szczesny?: string;
+  danilo?: string;
+  ligt?: string;
+  bonucci?: string;
+  alex?: string;
+  sandro?: string;
+  cuadrado?: string;
+  bentancur?: string;
+  locatelli?: string;
+  chiesa?: string;
+  dybala?: string;
+  ronaldo?: string;
+  vicario?: string;
+  stojanovic?: string;
+  romagnoli?: string;
+  ismajli?: string;
+  marchizza?: string;
+  ricci?: string;
+  stulac?: string;
+  haas?: string;
+  bajrami?: string;
+  bandinelli?: string;
+  mancuso?: string;
+  perin?: string;
+  pinsoglio?: string;
+  rugani?: string;
+  pellegrini?: string;
+  'lu.'?: string;
+  sciglio?: string;
+  '45%'?: string;
+  chiellini?: string;
+  bernardeschi?: string;
+  rabiot?: string;
+  kulusevski?: string;
+  mckennie?: string;
+  morata?: string;
+  brignoli?: string;
+  furlan?: string;
+  fiamozzi?: string;
+  luperto?: string;
+  crociata?: string;
+  damiani?: string;
+  zurkowski?: string;
+  henderson?: string;
+  cutrone?: string;
+  la?: string;
+  mantia?: string;
+  pinamonti?: string;
+  sirigu?: string;
+  biraschi?: string;
+  vanheusden?: string;
+  criscito?: string;
+  sabelli?: string;
+  sturaro?: string;
+  badelj?: string;
+  hernani?: string;
+  cambiaso?: string;
+  kallon?: string;
+  pandev?: string;
+  meret?: string;
+  lorenzo?: string;
+  manolas?: string;
+  koulibaly?: string;
+  mario?: string;
+  rui?: string;
+  lobotka?: string;
+  ruiz?: string;
+  politano?: string;
+  elmas?: string;
+  insigne?: string;
+  lozano?: string;
+  marchetti?: string;
+  vasquez?: string;
+  masiello?: string;
+  rovella?: string;
+  agudelo?: string;
+  cassata?: string;
+  melegoni?: string;
+  behrami?: string;
+  ekuban?: string;
+  favilli?: string;
+  bianchi?: string;
+  buksa?: string;
+  ospina?: string;
+  marfella?: string;
+  rrahmani?: string;
+  malcuit?: string;
+  juan?: string;
+  jesus?: string;
+  zanoli?: string;
+  zielinski?: string;
+  gaetano?: string;
+  petagna?: string;
+  consigli?: string;
+  toljan?: string;
+  chiriches?: string;
+  ferrari?: string;
+  'g.'?: string;
+  rogerio?: string;
+  lopez?: string;
+  'm.'?: string;
+  frattesi?: string;
+  'traore&#39;'?: string;
+  'hj.'?: string;
+  djuricic?: string;
+  boga?: string;
+  caputo?: string;
+  audero?: string;
+  bereszynski?: string;
+  yoshida?: string;
+  augello?: string;
+  ekdal?: string;
+  thorsby?: string;
+  candreva?: string;
+  verre?: string;
+  damsgaard?: string;
+  quagliarella?: string;
+  pegolo?: string;
+  kyriakopoulos?: string;
+  ayhan?: string;
+  muldur?: string;
+  peluso?: string;
+  matheus?: string;
+  henrique?: string;
+  magnanelli?: string;
+  haraslin?: string;
+  raspadori?: string;
+  defrel?: string;
+  scamacca?: string;
+  oddei?: string;
+  falcone?: string;
+  ravaglia?: string;
+  depaoli?: string;
+  chabot?: string;
+  tonelli?: string;
+  murillo?: string;
+  murru?: string;
+  adrien?: string;
+  silva?: string;
+  askildsen?: string;
+  caprari?: string;
+  maignan?: string;
+  calabria?: string;
+  tomori?: string;
+  kjaer?: string;
+  hernandez?: string;
+  't.'?: string;
+  tonali?: string;
+  bennacer?: string;
+  saelemaekers?: string;
+  diaz?: string;
+  'b.'?: string;
+  rafael?: string;
+  leao?: string;
+  giroud?: string;
+  radunovic?: string;
+  walukiewicz?: string;
+  godin?: string;
+  carboni?: string;
+  nandez?: string;
+  deiola?: string;
+  marin?: string;
+  strootman?: string;
+  dalbert?: string;
+  joao?: string;
+  pavoletti?: string;
+  tatarusanu?: string;
+  plizzari?: string;
+  'ballo-toure&#39;'?: string;
+  kalulu?: string;
+  gabbia?: string;
+  florenzi?: string;
+  krunic?: string;
+  castillejo?: string;
+  maldini?: string;
+  pellegri?: string;
+  rebic?: string;
+  aresti?: string;
+  ceppitelli?: string;
+  altare?: string;
+  lykogiannis?: string;
+  zappa?: string;
+  pereiro?: string;
+  grassi?: string;
+  farias?: string;
+  belec?: string;
+  bogdan?: string;
+  gyomber?: string;
+  jaroszynski?: string;
+  kechrida?: string;
+  coulibaly?: string;
+  tacchio?: string;
+  capezzi?: string;
+  ruggeri?: string;
+  simy?: string;
+  bonazzoli?: string;
+  patricio?: string;
+  karsdorp?: string;
+  mancini?: string;
+  ibanez?: string;
+  vina?: string;
+  cristante?: string;
+  veretout?: string;
+  el?: string;
+  shaarawy?: string;
+  'lo.'?: string;
+  mkhitaryan?: string;
+  abraham?: string;
+  fiorillo?: string;
+  aya?: string;
+  zortea?: string;
+  schiavone?: string;
+  kastanos?: string;
+  obi?: string;
+  kristoffersen?: string;
+  djuric?: string;
+  fuzato?: string;
+  boer?: string;
+  calafiori?: string;
+  kumbulla?: string;
+  reynolds?: string;
+  diawara?: string;
+  darboe?: string;
+  villar?: string;
+  shomurodov?: string;
+  mayoral?: string;
+  carles?: string;
+  perez?: string;
+}
+
+export interface DIV {
+  type: string;
+  SPAN: string[];
+  attributes: Attributes6;
+}
+
+export interface Attributes6 {
+  class: string;
+}
+
+export interface Attributes5 {
+  class: string;
+  'role-p'?: string;
+  'player-percentage'?: string;
+  good?: string;
+  'role-d'?: string;
+  warn?: string;
+  'role-c'?: string;
+  'role-a'?: string;
+}
+
+export interface Attributes4 {
+  class: string;
+  href?: string;
+  'label-danger'?: string;
+  'label-warning'?: string;
+  'label-info'?: string;
+}
+
+export interface Attributes3 {
+  class: string;
+  'mb-1'?: string;
+  'd-flex'?: string;
+  'pitch-frame'?: string;
+  box?: string;
+  'player-list'?: string;
+  home?: string;
+  away?: string;
+}
+
+export interface HEADER {
+  type: string;
+  SMALL: (SMALL | string)[];
+  attributes: Attributes2;
+}
+
+export interface Attributes2 {
+  '2021': string;
+  '27'?: string;
+  class: string;
+  'ml-auto': string;
+  'match-date': string;
+  itemprop: string;
+  content: string;
+  agosto: string;
+  '28'?: string;
+  '29'?: string;
+}
+
+export interface SMALL {
+  type: string;
+  attributes: Attributes;
+}
+
+export interface Attributes {
+  class: string;
+  'fa-clock-o': string;
+}
